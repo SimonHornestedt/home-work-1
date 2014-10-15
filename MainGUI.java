a521a0ab4fae495430134a7fb1e54358c8c069f2
SimonHornestedt <simon_hornestedt17@hotmail.com>
2014-10-15 14:41:42 +0200

    Uppdaterad GUI klass

--- MainGUI.java
+++ MainGUI.java
@@ -1,4 +1,26 @@
-public MainGUI{
+
+import javax.swing.*;
+
+public class MainGUI extends JFrame{
+
+    public MainGUI(){
+        initGUI();
+    }
+    
+    
+    private void initGUI(){
+
+    }
+    
+    public static void main (String[]args){
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                new MainGUI().setVisible(true);
+            }
+        });
+    }
+}
 
 
-{
+
+
